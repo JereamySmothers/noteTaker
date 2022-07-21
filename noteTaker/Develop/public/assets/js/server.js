ddf5a6b883a43333646c284b7f0ed2eb57e77584
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-07-20 18:26:18 -0700

    finished server.js

--- noteTaker/Develop/public/assets/js/server.js
+++ noteTaker/Develop/public/assets/js/server.js
@@ -7,3 +7,18 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// express data parsing
+app.use(express.urlencoded());
+
+// express data conversion to json
+app.use(express.json());
+app.use(express.static(__dirname + '/'));
+
+// api and html routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// initiates server response on PORT
+app.listen(PORT, (console) => {
+    console.log(PORT);
+});
