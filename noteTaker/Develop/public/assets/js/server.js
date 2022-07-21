45ae62349950a040534548d5bf3f154f42bc16a5
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-07-20 18:18:28 -0700

    added basics of server.js and routes folder with api and html route folders

--- noteTaker/Develop/public/assets/js/server.js
+++ noteTaker/Develop/public/assets/js/server.js
@@ -1,1 +1,9 @@
+// npm package and local dependencies
 const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// initializing express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
